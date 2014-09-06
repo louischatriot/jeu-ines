b550a699769e8b2224d4da5512ded1b6db21997d
Louis Chatriot <louis.chatriot@gmail.com>
2014-09-06 18:29:55 +0200

    Saving data

--- assets/js/play.js
+++ assets/js/play.js
@@ -1,11 +1,32 @@
-console.log("Beginning play");
+console.log("Beginning play - Initializing data");
+if (!localStorage.myAnswers) { localStorage.myAnswers = {}; }
+if (!localStorage.goodAnswers) { localStorage.goodAnswers = {}; }
+
+var currentQuestion = null
+  , currentState = 'NOT_STARTED'
+  ;
+
 
 var socket = io();
 
-
-socket.on('question', function (data) {
+// Starting a new question
+socket.on('question.start', function (data) {
   console.log("================");
   console.log(data);
 });
+
+
+socket.on('game.status', function (data) {
+  console.log("======================");
+  console.log("Received new status");
+  console.log(data);
+});
+
+
+function startCurrentQuestion () {
+  console.log("=====================");
+  console.log("Starting current question");
+  console.log(currentQuestion);
+}
 
 
