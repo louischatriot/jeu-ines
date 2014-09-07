a08bd7b664eafb74c25181f7fa22dfe50f3e3988
Louis Chatriot <louis.chatriot@gmail.com>
2014-09-07 22:50:05 +0200

    Can ensure a player is logged and can submit an answer

--- assets/js/play.js
+++ assets/js/play.js
@@ -12,19 +12,29 @@ var currentQuestion = null
 // Asynchronous function which ensures this player is logged and recorded in the database
 // If he's not, API gets called, creates a new player and sends back the id
 // Of course nothing is secured in this kind of environment
-// callback signature: (err, playerId)
+// callback signature: (err)
 function ensurePlayerIsLogged (cb) {
-  var callback = cb || function () {};
-
+  var callback = function (jqXHR) {
+    if (jqXHR.status === 200) {
+      localStorage.setItem('playerId', JSON.parse(jqXHR.response)._id);
+      console.log('INFO - Player logged as id ' + localStorage.getItem('playerId'));
+      if (cb) { return cb(null); }
+    } else {
+      if (cb) { return cb({ getPlayerFail: true }); }
+    }
+  };
+    
   console.log('INFO - Ensuring player is logged');
 
   if (localStorage.getItem('playerId')) {
-    $.ajax({ url: '/player/' + localStorage.getItem('playerId') });
+    $.ajax({ url: '/player/' + localStorage.getItem('playerId')
+           , complete: callback
+           });
   } else {
-    $.ajax({ url: '/player/' });
+    $.ajax({ url: '/player/'
+           , complete: callback
+           });
   }
-  
-
 }
 
 
@@ -37,6 +47,11 @@ actions['NOT_STARTED'] = function (data) {
 actions['QUESTION_ASKED'] = function (data) {
   var templateData = { question: data.currentQuestion };
   $('#display-pannel').html(Mustache.render($('#question-asked').html(), templateData));
+  $('#display-pannel .answer').on('click', function (event) {
+    var $target = $(event.target);
+    $('#display-pannel .answer').removeClass('selected');
+    $target.addClass('selected');
+  });
 };
 
 
@@ -45,4 +60,12 @@ socket.on('game.status', function (data) {
   console.log(data);
   actions[data.currentStatus](data);
 });
+
+
+
+
+
+
+
+
 
